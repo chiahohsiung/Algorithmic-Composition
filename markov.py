2b9c225de743047150dc39ae1014da8d8a11a1b3
chiahohsiung <ch3472@columbia.edu>
2020-12-30 12:38:22 -0500

    refactored

--- markov.py
+++ markov.py
@@ -1,13 +1,7 @@
-import numpy
 import random
 
 
 # markov chain
-
-f = open('tongue_twister', 'r')
-text = f.read().lower()
-# print(text)
-
 # step 1 all the ngrams count all the following char for every ngram
 # step 2 generate 
 
@@ -17,38 +11,44 @@ text = f.read().lower()
 # hel ell llo
 
 # step 1 and 2
-order = 3
+def gen_possiblities(text, order):
+	ngram_chars = {}
+	for i in range(len(text) - order):
+		ngram = text[i:i+order]
 
-ngram_chars = {}
-for i in range(len(text) - order):
-	ngram = text[i:i+order]
+		if ngram not in ngram_chars:
+			ngram_chars[ngram] = [text[i+order]]
+		else:
+			ngram_chars[ngram].append(text[i+order])
 
-	if ngram not in ngram_chars:
-		ngram_chars[ngram] = [text[i+order]]
-	else:
-		ngram_chars[ngram].append(text[i+order])
-# sorted_ngram_chars = {k: v for k, v in sorted(ngram_chars.items(), key=lambda item: len(item[1]))}	
-# for k, v in sorted_ngram_chars.items():
-# 	print(k, v)
-# 	break
-print(ngram_chars)
+	return ngram_chars
+
 # generate
-result = 'pet'
-cur_word = result
-iterations = 100
-for i in range(iterations):
-	# print('cur_word', cur_word)
-	if cur_word not in ngram_chars:
-		break
-	
-	char_ls = ngram_chars[cur_word]
-	if not char_ls:	
-		break
-	
-	char = random.choice(char_ls)
-	result += char
-	cur_word = result[-order:]
+def gen_sequence(order, ngram_chars, start_word, iterations):
+	cur_word = start_word
+	for i in range(iterations):
+		# print('cur_word', cur_word)
+		if cur_word not in ngram_chars:
+			break
+		
+		char_ls = ngram_chars[cur_word]
+		if not char_ls:	
+			break
+		
+		char = random.choice(char_ls)
+		start_word += char
+		cur_word = start_word[-order:]
 
-print(result)
+	return start_word
+
+
+if __name__ == '__main__':
+	f = open('tongue_twister', 'r')
+	text = f.read().lower()
+	order = 3
+	ngram_chars = gen_possiblities(text, order)
+	start_word = 'pet'
+	result = gen_sequence(order, ngram_chars, start_word, 100)
+	print(result)
 
 
