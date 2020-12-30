f480fefc92cb79511d2a8680e47a030c42575e2c
chiahohsiung <ch3472@columbia.edu>
2020-12-29 23:41:07 -0500

    add steps

--- markov.py
+++ markov.py
@@ -5,3 +5,11 @@ import numpy
 f = open('test_text', 'r')
 text = f.read()
 print(text)
+# step 1 all the ngrams
+# step 2 count all the following char for every ngram
+# step 3 generate 
+
+# example 
+# hello word
+# ngram order <- 3
+
