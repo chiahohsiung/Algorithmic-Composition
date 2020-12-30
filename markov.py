2594ce9c871451235f3452221f9090b516b0f682
chiahohsiung <ch3472@columbia.edu>
2020-12-30 12:30:03 -0500

    add generate

--- markov.py
+++ markov.py
@@ -1,10 +1,13 @@
 import numpy
+import random
+
 
 # markov chain
 
-f = open('test_text', 'r')
-text = f.read()
-print(text)
+f = open('tongue_twister', 'r')
+text = f.read().lower()
+# print(text)
+
 # step 1 all the ngrams count all the following char for every ngram
 # step 2 generate 
 
@@ -14,7 +17,7 @@ print(text)
 # hel ell llo
 
 # step 1 and 2
-order = 6
+order = 3
 
 ngram_chars = {}
 for i in range(len(text) - order):
@@ -24,8 +27,28 @@ for i in range(len(text) - order):
 		ngram_chars[ngram] = [text[i+order]]
 	else:
 		ngram_chars[ngram].append(text[i+order])
-	
+# sorted_ngram_chars = {k: v for k, v in sorted(ngram_chars.items(), key=lambda item: len(item[1]))}	
+# for k, v in sorted_ngram_chars.items():
+# 	print(k, v)
+# 	break
 print(ngram_chars)
+# generate
+result = 'pet'
+cur_word = result
+iterations = 100
+for i in range(iterations):
+	# print('cur_word', cur_word)
+	if cur_word not in ngram_chars:
+		break
+	
+	char_ls = ngram_chars[cur_word]
+	if not char_ls:	
+		break
+	
+	char = random.choice(char_ls)
+	result += char
+	cur_word = result[-order:]
 
+print(result)
 
 
