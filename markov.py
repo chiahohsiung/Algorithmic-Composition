b40f13554f3b5d62037f645b47848d7f9e430145
chiahohsiung <ch3472@columbia.edu>
2020-12-29 23:59:00 -0500

    ngrams

--- markov.py
+++ markov.py
@@ -5,11 +5,27 @@ import numpy
 f = open('test_text', 'r')
 text = f.read()
 print(text)
-# step 1 all the ngrams
-# step 2 count all the following char for every ngram
-# step 3 generate 
+# step 1 all the ngrams count all the following char for every ngram
+# step 2 generate 
 
 # example 
 # hello word
 # ngram order <- 3
+# hel ell llo
+
+# step 1 and 2
+order = 6
+
+ngram_chars = {}
+for i in range(len(text) - order):
+	ngram = text[i:i+order]
+
+	if ngram not in ngram_chars:
+		ngram_chars[ngram] = [text[i+order]]
+	else:
+		ngram_chars[ngram].append(text[i+order])
+	
+print(ngram_chars)
+
+
 
